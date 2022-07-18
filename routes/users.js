e082f2581b06530d8bb9dc3e686f6eade63e40a6
Joshua Semana <jsemana054@gmail.com>
2022-07-17 20:38:14 -0600

    added most CRUD applications

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+  next();
+});
+router.get("/cool", function (res, res, next) {
+  res.send("You're so cool!");
+});
+
+module.exports = router;
